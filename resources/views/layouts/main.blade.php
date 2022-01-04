f789a566c3ba16d9c01619cfb8bb10b91b9a3245
Vitor Sanches <vitsanches@live.com>
2022-01-03 21:37:11 -0300

    criando eventos feito

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -41,10 +41,19 @@
                 </div>
             </nav>
         </header>
-            @yield('content')
-            <footer>
+        <main>
+            <div class="container-fluid">
+                <div class="row">
+                    @if(session('msg'))
+                    <p class="msg">{{ session('msg')}}</p>
+                    @endif
+                    @yield('content')
+                </div>
+            </div>
+        </main>  
+        <footer>
                 <p>Sanches Eventos &copy; 2021</p>
-            </footer>
+        </footer>
             <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,28 @@
 
 @section('content')
 
-@foreach($events ?? '' as $event) 
-    <p>{{$event->title}} -- {{$event->description}}</p>
-@endforeach
+<div id="search-container" class="col md-12">
+    <h1>Busque um Evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2>Proximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos proximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach($events as $event)
+        <div class="card col-md-3">
+            <img src="/img/evento1.jpg" alt="{{ $event->title }}">
+            <dic class="card-body">
+                <p class="card-date">10/09/2020</p>
+                <h5 class="card-title">{{ $event->title}}</h5>
+                <p class="card-participants">x Participantes</p>
+                <a href="#" class="btn btn-primary">Saber Mais</a>
+            </dic>
+        </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
